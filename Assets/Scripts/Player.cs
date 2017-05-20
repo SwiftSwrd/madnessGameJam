7f3cd61032b6f70ce9500b4747ba35506ed14a58
SwiftSwrd <bjk386@mail.usask.ca>
2017-05-19 21:28:17 -0600

    Input reading from controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+  private const float DEADZONE = 0.4f;
+
+  private bool right;
+  private bool left;
+  private bool up;
+  private bool down;
+
+  private bool aDown;
+  private bool lDown;
+  private bool rDown;
+
   // Use this for initialization
   void Start() {
   	
   }
-  
+
   // Update is called once per frame
   void Update() {
   	
+  }
+
+  // FixedUpdate is called once per physics frame
+  void FixedUpdate() {
+    holdingUp();
+    holdingDown();
+    holdingRight();
+    holdingLeft();
+    buttonPressed("A", ref aDown);
+    buttonPressed("L", ref lDown);
+    buttonPressed("R", ref rDown);
+  }
+
+  private bool holdingUp() {
+    up = DEADZONE < Input.GetAxis("Vertical");
+    return up;
+  }
+
+  private bool holdingDown() {
+    down = -DEADZONE > Input.GetAxis("Vertical");
+    return down;
+  }
+
+  private bool holdingRight() {
+    right = DEADZONE < Input.GetAxis("Horizontal");
+    return right;
+  }
+
+  private bool holdingLeft() {
+    left = -DEADZONE > Input.GetAxis("Horizontal");
+    return left;
+  }
+
+  private bool buttonPressed(string name, ref bool heldDown) {
+    if (1 == Input.GetAxisRaw(name)) {
+      if (true == heldDown)
+        return false;
+      else {
+        heldDown = true;
+        return true;
+      }
+    }
+    else {
+      if (true == heldDown)
+        heldDown = false;
+      return false;
+    }
   }
 }
