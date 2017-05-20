72032bd7904c8a8db489c99fa59a3b28e5e3a56e
SwiftSwrd <bjk386@mail.usask.ca>
2017-05-19 20:25:12 -0600

    basic scene to get MVP working in

---
+++ Assets/Scripts/CameraInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInitializer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    Camera.main.aspect = 16f/9f;
+    //Camera.main.orthographicSize = (1.54f * (Screen.height / Screen.width));
+  }
+}
