60936b71dc745f9b23c933e1802f61b2cdbd3dd8
SwiftSwrd <bjk386@mail.usask.ca>
2017-05-20 21:53:56 -0600

    Finished test scene, started on a level.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraInitializer : MonoBehaviour {
+public class CameraControl : MonoBehaviour {
+
+  public GameObject lightChar;
 
 	// Use this for initialization
 	void Start () {
     Camera.main.aspect = 16f/9f;
     //Camera.main.orthographicSize = (1.54f * (Screen.height / Screen.width));
+  }
+
+  void Update() {
+    Vector3 pos = lightChar.transform.position;
+    pos.y += 4.5f;
+    pos.z = -1;
+    transform.position = pos;
   }
 }
---
+++ Assets/Scripts/HorizFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizFollow : MonoBehaviour {
+
+  public GameObject lightChar;
+
+  void Update() {
+    Vector3 pos = lightChar.transform.position;
+    pos.y = transform.position.y;
+    pos.z = 0;
+    transform.position = pos;
+  }
+}
